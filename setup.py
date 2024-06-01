fc5accc22e5ed6eadd6159ce9df59b0fdb9ebe3d
kristoffervatnehol <kristoffer.vatnehol@gmail.com>
2024-06-01 12:05:23 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-
 from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='optimized-utility-library',
@@ -17,12 +20,12 @@ setup(
             'flake8'
         ]
     },
-    author='Kristoffer Vatanehol',
-    author_email='your.kristoffer.vatnehol@gmail.com',
+    author='Kristoffer Vatnehol',
+    author_email='kristoffer.vatnehol@gmail.com',
     description='An optimized utility library for directory, file, image, and JSON operations.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/optimized-utility-library',  # Update with your GitHub repo URL
+    url='https://github.com/kristofferv98/optimized-utility-library',  # Update with your GitHub repo URL
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
